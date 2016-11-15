544ec1a4e3d1383bb14d674185e1c17aa8d7e2b5
kr0705 <kratigyarastogi0705@gmail.com>
2016-11-15 22:56:48 +0530

    fixes errors

--- project1/settings.py
+++ project1/settings.py
@@ -96,7 +96,7 @@ DATABASES = {'default': dj_database_url.config()}
 # 		#'ENGINE': 'django.db.backends.sqlite3',
 # 		#'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 # 	}
-}
+# }
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
